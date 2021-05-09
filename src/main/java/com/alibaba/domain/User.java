b58a3fae5c7120a1affd72207eca3d15e7850cbc
zs13337108334 <zs13337108334@163.com>
2021-05-09 12:51:27 +0800

    demo

---
+++ src/main/java/com/alibaba/domain/User.java
@@ -0,0 +1,35 @@
+package com.alibaba.domain;
+
+import org.springframework.context.annotation.Bean;
+
+
+public class User {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAgr() {
+        return agr;
+    }
+
+    public void setAgr(String agr) {
+        this.agr = agr;
+    }
+
+    public User() {
+    }
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private String agr;
+}
