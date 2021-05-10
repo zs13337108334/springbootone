61554bc2bac636ca76f0bf6ab235be00f4b1be13
zs13337108334 <zs13337108334@163.com>
2021-05-11 00:35:12 +0800

    代码标准化

--- src/main/java/com/alibaba/domain/User.java
+++ src/main/java/com/alibaba/domain/User.java
@@ -1,9 +1,7 @@
 package com.alibaba.domain;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.context.annotation.Bean;
 
 @Getter
 @Setter
--- src/main/java/com/alibaba/service/OrderService.java
+++ src/main/java/com/alibaba/service/OrderService.java
@@ -2,7 +2,6 @@ package com.alibaba.service;
 
 import com.alibaba.dao.OrderDao;
 import com.alibaba.domain.User;
-import com.alibaba.fastjson.JSON;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +15,11 @@ public class OrderService {
     private OrderDao orderDao;
 
     public User OrderDetailById(int id) {
-        if(id != 0){
+        if (id != 0) {
             log.info("OrderDetailById param={} 查询成功");
             return orderDao.findById(id);
         }
         log.error("OrderDetailById param={} 查询失败！");
-        return new User("查询","失败");
+        return new User("查询", "失败");
     }
 }
